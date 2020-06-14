3a341f274011cc426ae18c7928fb7521ba3a2144
Curiosity <66764822+Curiosity-GitHub@users.noreply.github.com>
2020-06-14 17:33:39 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+game 'gta5'
+
+description 'FiveM Loadingscreen [DRP]'
+
+files {
+    'index.html',
+    'css/style.css',
+    'script.js',
+    'img/logo.png',
+    'music/loading.ogg'
+}
+
+loadscreen 'index.html'
